258fb71cd6bdf6c06f765ddf73bddc88864abbfc
5undel <max.sundel@Choice.se>
2022-08-17 06:35:44 +0000

    Add eventlistener and function to number button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 class Calculator {
-    constructor(prevOperandTextElement. currentOperandTextElement) {
+    constructor(prevOperandTextElement, currentOperandTextElement) {
         this.prevOperandTextElement = prevOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
     clear() {
-
+        this.currentOperand = ''
+        this.prevOperand = ''
+        this.operation = undefined
     }
 
     delete() {
@@ -13,7 +16,8 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        if( number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
@@ -25,7 +29,7 @@ class Calculator {
     }
 
     updateDisplay() {
-        
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 
 }
@@ -39,3 +43,11 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const prevOperandTextElement = document.querySelector('[data-prev-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+const calculator = new Calculator(prevOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
